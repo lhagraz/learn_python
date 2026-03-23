32e738d64a3bf59942d9802a4ca02fc9eaf888b3
Lhagraz <Lhagraz@gmail.com>
2026-03-23 15:36:21 -0300

    Primer commit de learn_python

---
+++ M1/m1_ej01.py
@@ -0,0 +1,22 @@
+# **Ejercicio 1 — Variables de reconocimiento**
+'''Declara variables para almacenar los datos típicos de un objetivo de reconocimiento: dirección IP, nombre de 
+dominio, sistema operativo detectado, número de puertos abiertos y si el host está activo. Imprime cada variable 
+con una etiqueta descriptiva usando f-strings.'''
+direccion_ip = "192.168.1.105"
+dominio = "servidor-interno.empresa.com"
+sistema_op = "Windows Server 2019" 
+nro_puertos = 5
+estado_host = False
+estado_legible = "Activo" if estado_host else "Inactivo"
+
+print(f"""
+=========================================
+Datos del objetivo
+=========================================
+Dirección IP: {direccion_ip}
+Dominio: {dominio}
+Sistema operativo: {sistema_op}
+Número de puertos abiertos: {nro_puertos}
+Estado del host: {estado_legible}
+=========================================
+""")
---
+++ M1/m2_ej02.py
@@ -0,0 +1,7 @@
+#**Ejercicio 2 — Calculadora de subredes**
+'''El usuario ingresa el número de hosts necesarios en una subred. 
+Calcula y muestra cuántos bits se necesitan para representar esa cantidad de hosts 
+(usa logaritmo base 2 aproximado con potencias de 2), y cuál es el rango total de IPs 
+disponibles con ese número de bits. Pista: investiga cómo usar `2 ** n` para encontrar 
+el valor mínimo mayor al número ingresado.
+'''
