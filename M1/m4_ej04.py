2a2d9ee567a9242f18de24420cdc79241b4ddacc
Lhagraz <Lhagraz@gmail.com>
2026-03-23 20:07:04 -0300

    se añadio ejercicio numero 4

---
+++ M1/m4_ej04.py
@@ -0,0 +1,7 @@
+# **Ejercicio 4 — Validador básico de IP**
+'''El usuario ingresa una dirección IP como string. 
+Verifica e imprime si el string contiene exactamente 3 puntos 
+(condición necesaria pero no suficiente para ser una IPv4 válida).
+ Muestra también la longitud del string y cuántas veces aparece cada 
+ número del 0 al 9 en la dirección. No uses expresiones regulares.
+'''
