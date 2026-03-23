687cf93ec9030652ee9ce2e9e788bd0b4ca8cda8
Lhagraz <Lhagraz@gmail.com>
2026-03-23 16:28:38 -0300

    completado ejercicio numeros dos

--- M1/m2_ej02.py
+++ M1/m2_ej02.py
@@ -5,3 +5,20 @@ Calcula y muestra cuántos bits se necesitan para representar esa cantidad de ho
 disponibles con ese número de bits. Pista: investiga cómo usar `2 ** n` para encontrar 
 el valor mínimo mayor al número ingresado.
 '''
+cantidad_host = int(input("Ingrese la cantidad de hosts necesarios: "))
+
+bits = 1
+total_ips = 2 ** bits
+
+while total_ips < cantidad_host:
+    bits += 1
+    total_ips = 2 ** bits
+    
+host_utilizables = total_ips - 2
+
+
+print("=" * 40) 
+print(f"Bits necesarios: {bits}")  
+print(f"IPs totales en subred: {total_ips}")  
+print(f"Host utilizables: {host_utilizables}")  
+print("=" * 40) 
