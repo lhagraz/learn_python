b485a8d8d01260687e1017b020fb05dcdab5bd5e
Lhagraz <Lhagraz@gmail.com>
2026-03-24 13:51:28 -0300

    logica y estrutura inicial solucion del ejercicio 4

--- M1/m4_ej04.py
+++ M1/m4_ej04.py
@@ -5,3 +5,27 @@ Verifica e imprime si el string contiene exactamente 3 puntos
  Muestra también la longitud del string y cuántas veces aparece cada 
  número del 0 al 9 en la dirección. No uses expresiones regulares.
 '''
+ip = input("Ingrese dirección IP: ")
+
+cant_puntos = ip.count(".")
+
+longitud = len(ip)
+
+if cant_puntos == 3:
+    print("Contiene 3 puntos")
+
+elif cant_puntos < 3:
+    print("Error tiene menos puntos que la cantidad requerida")
+else:
+    print("Error tiene mas puntos que la cantidad requerida")
+
+for i in "0123456789":
+    conteo = ip.count(i)
+    if conteo > 0:
+        print(f"Se encontro {conteo} '{i}' en la IP.")
+
+    
+
+
+
+
