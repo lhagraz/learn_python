f2ca7076616c493b6ee08a871f0336b59eb1d5b3
Lhagraz <Lhagraz@gmail.com>
2026-03-30 16:58:49 -0300

    se añadio  ejecicio 7

---
+++ M1/m1_ej07.py
@@ -0,0 +1,6 @@
+#**Ejercicio 7 — Calculadora de entropía básica**
+'''La entropía de una contraseña depende del conjunto de 
+caracteres usado y su longitud. Dado un set de caracteres posibles (input: número entero) 
+y la longitud de la contraseña (input: entero), calcula el número total de combinaciones
+ posibles con la fórmula `combinaciones = set_size ** length`. Muestra el resultado y evalúa 
+ si supera 1 billón (`10 ** 12`) imprimiendo `True` o `False`.'''
