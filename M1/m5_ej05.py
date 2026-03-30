a68c07811a14584ac95c524a16a42628676a1841
Lhagraz <Lhagraz@gmail.com>
2026-03-30 14:39:54 -0300

    correccion nombre del modulo y adicción del ejercicio 5

---
+++ M1/m5_ej05.py
@@ -0,0 +1,8 @@
+#**Ejercicio 5 — Conversor de unidades de datos**
+'''El usuario ingresa un tamaño en bytes. E
+l programa debe mostrar ese valor convertido a: 
+Kilobytes (KB), Megabytes (MB), Gigabytes (GB) y
+ Terabytes (TB). Usa división y redondea a 4 decimales con 
+ `round()`. Esto es útil al analizar tamaño de archivos capturados en una auditoría.
+'''
+tamaño_bytes = int(input("Ingrese tamaño a convertir en bytes: "))
